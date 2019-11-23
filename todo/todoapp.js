ece3db71e25b24cd1e6d269fa2f5cf168d6ae4e9
Richard <51269862+MeRichard123@users.noreply.github.com>
2019-11-23 18:20:40 +0000

    Update todoapp.js

--- todo/todoapp.js
+++ todo/todoapp.js
@@ -1,5 +1,7 @@
 //Time
 const time = document.querySelector(".navbar-brand");
+var inputValue = document.getElementById("myInput").value;
+var UserName = localStorage.getItem("username");
 
 function printTime() {
   var d = new Date();
@@ -19,3 +21,90 @@ function printTime() {
   time.textContent = hours + ":" + Nmins + ":" + Nsec;
 }
 setInterval(printTime, 1000);
+
+window.addEventListener("load", () => {
+  Object.keys(localStorage).forEach(function(key) {
+    var li = document.createElement("li");
+    li.id = "list-item";
+    var item = localStorage.getItem(key);
+    if (item !== UserName) {
+      t = document.createTextNode(item);
+      li.appendChild(t);
+      const TODOLIST = document.getElementById("myUl");
+      TODOLIST.appendChild(li);
+    }
+  });
+
+  // Create a "close" button and append it to each list item
+  var myNodelist = document.querySelectorAll("#list-item");
+  var i;
+  for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+  }
+
+  // Click on a close button to hide the current list item
+  var close = document.querySelectorAll(".close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+      let key = div.textContent.slice(0, -1);
+      localStorage.removeItem(key);
+      console.log(localStorage);
+    };
+  }
+
+  // Add a "checked" symbol when clicking on a list item
+  var list = document.querySelector("#myUl");
+  list.addEventListener(
+    "click",
+    evt => {
+      if (evt.target.tagName === "LI") {
+        evt.target.classList.toggle("checked");
+      }
+    },
+    false
+  );
+  const addBtn = document.querySelector(".addBtn");
+  addBtn.addEventListener("click", () => {
+    newElement();
+  });
+
+  // Create a new list item when clicking on the "Add" button
+  function newElement() {
+    var li = document.createElement("li");
+    li.id = "list-item";
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    localStorage.setItem(inputValue, inputValue);
+    if (inputValue === "") {
+      alert("You must write something!");
+    } else {
+      const TODOLIST = document.getElementById("myUl");
+      TODOLIST.appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    var close = document.querySelectorAll(".close");
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+        let key = div.textContent.slice(0, -1);
+        localStorage.removeItem(key);
+      };
+    }
+  }
+});
