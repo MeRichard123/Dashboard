66db9c0d26a68c18bae52ab4bf558f30d54f6891
Richard <51269862+MeRichard123@users.noreply.github.com>
2019-12-05 12:00:52 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const arrow = document.querySelector(".arrow");
 const textBox = document.querySelector(".text_box");
 
 window.addEventListener("load", () => {
-  textBox.addEventListener("input", (e) => {
-    e.preventDefault()
+  textBox.addEventListener("input", () => {
     arrow.classList.remove("inactive");
   });
   arrow.addEventListener("click", () => {
     const name = textBox.value;
     localStorage.clear();
     localStorage.setItem("username", name);
+  });
+  textBox.keyup(function (event) {
+    if (event.which === 13 || event.keyCode === 13) {
+        event.preventDefault();
   });
 });
